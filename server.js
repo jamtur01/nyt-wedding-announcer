d525a68e8da12d4872075fe9b4ff2cd67c7682dd
Glitch (local-gourd) <none>
2019-11-20 21:20:07 +0000

    🙉😇 Checkpoint ./server.js:5117979/39

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.get('/', function(req, res) {
 app.post("/", function(req, res) {
   var results = generate();
   res.send(results);
-  res.end();
+  res.redirect('back');
 });
 
 // listen for requests :)
