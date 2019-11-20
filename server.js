09dd98dcc34df2f5cabd7cff7a3581f167fec96e
Glitch (local-gourd) <none>
2019-11-20 22:07:16 +0000

    🎗🐆 Checkpoint ./server.js:5117979/26991

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 
 var tracery = require("tracery-grammar");
 var faker = require("faker");
@@ -105,70 +105,79 @@ const postgrad_config = {
   length: 1
 };
 
-
-function generate () {
-var brideName = fakerator.names.nameF();
-var brideMum = fakerator.names.nameF();
-var brideDad = fakerator.names.nameM();
-var brideJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var brideFormerJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var brideMotherJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var brideFatherJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var bridePlace = fakerator.populate("#{address.city}");
-var brideGrad = uniqueNamesGenerator(ivy_config);
-var brideGrad2 = uniqueNamesGenerator(ivy_config);
-var bridePostGrad = uniqueNamesGenerator(postgrad_config);
-var groomName = faker.name.findName(); 
-var groomGrad = uniqueNamesGenerator(ivy_config);
-var groomDad = fakerator.names.nameM();
-var groomMum = fakerator.names.nameF();
-var groomJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var groomFatherJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var groomMotherJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
-var groomPlace = fakerator.populate("#{address.city}");
-var state = faker.address.state();
-var age = (Math.floor(Math.random() * 30) + 21).toString();
-var officiant = fakerator.names.nameM();
-var offRelationship = uniqueNamesGenerator(offrel_config);
-var wherePlace = uniqueNamesGenerator(placemet_config);
-var whereLocation = fakerator.populate("#{address.city}");
-var wherePlace2 = uniqueNamesGenerator(placemet_config);
-var whereLocation2 = fakerator.populate("#{address.city}");
-var place = uniqueNamesGenerator(places_config);
+function generate() {
+  var brideName = fakerator.names.nameF();
+  var brideMum = fakerator.names.nameF();
+  var brideDad = fakerator.names.nameM();
+  var brideJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
+  var brideFormerJob = faker.fake(
+    "{{name.jobTitle}} at {{company.companyName}}"
+  );
+  var brideMotherJob = faker.fake(
+    "{{name.jobTitle}} at {{company.companyName}}"
+  );
+  var brideFatherJob = faker.fake(
+    "{{name.jobTitle}} at {{company.companyName}}"
+  );
+  var bridePlace = fakerator.populate("#{address.city}");
+  var brideGrad = uniqueNamesGenerator(ivy_config);
+  var brideGrad2 = uniqueNamesGenerator(ivy_config);
+  var bridePostGrad = uniqueNamesGenerator(postgrad_config);
+  var groomName = faker.name.findName();
+  var groomGrad = uniqueNamesGenerator(ivy_config);
+  var groomDad = fakerator.names.nameM();
+  var groomMum = fakerator.names.nameF();
+  var groomJob = faker.fake("{{name.jobTitle}} at {{company.companyName}}");
+  var groomFatherJob = faker.fake(
+    "{{name.jobTitle}} at {{company.companyName}}"
+  );
+  var groomMotherJob = faker.fake(
+    "{{name.jobTitle}} at {{company.companyName}}"
+  );
+  var groomPlace = fakerator.populate("#{address.city}");
+  var state = faker.address.state();
+  var age = (Math.floor(Math.random() * 30) + 21).toString();
+  var officiant = fakerator.names.nameM();
+  var offRelationship = uniqueNamesGenerator(offrel_config);
+  var wherePlace = uniqueNamesGenerator(placemet_config);
+  var whereLocation = fakerator.populate("#{address.city}");
+  var wherePlace2 = uniqueNamesGenerator(placemet_config);
+  var whereLocation2 = fakerator.populate("#{address.city}");
+  var place = uniqueNamesGenerator(places_config);
 
   // This is the Tracery grammar.
-let notes = {
-  bride: [brideName],
-  groom: [groomName],
-  age: [age],
-  place: [place],
-  state: [state],
-  officant: [officiant],
-  offrel: [offRelationship],
-  bridejob: [brideJob],
-  bridplace: [bridePlace],
-  brideformer: [brideFormerJob],
-  bridegrad: [brideGrad],
-  bridegrad2: [brideGrad2],
-  bridepostgrad: [bridePostGrad],
-  bridemum: [brideMum],
-  bridedad: [brideDad],
-  brideplace: [bridePlace],
-  bridefatherjob: [brideFatherJob],
-  bridemotherjob: [brideMotherJob],
-  groomjob: [groomJob],
-  groomgrad: [groomGrad],
-  groommum: [groomMum],
-  groomdad: [groomDad],
-  groomplace: [groomPlace],
-  groommotherjob: [groomMotherJob],
-  groomfatherjob: [groomFatherJob],
-  whereplace: [wherePlace],
-  wherelocation: [whereLocation],
-  whereplace2: [wherePlace2],
-  wherelocation2: [whereLocation2],
-  origin: [
-    `
+  let notes = {
+    bride: [brideName],
+    groom: [groomName],
+    age: [age],
+    place: [place],
+    state: [state],
+    officant: [officiant],
+    offrel: [offRelationship],
+    bridejob: [brideJob],
+    bridplace: [bridePlace],
+    brideformer: [brideFormerJob],
+    bridegrad: [brideGrad],
+    bridegrad2: [brideGrad2],
+    bridepostgrad: [bridePostGrad],
+    bridemum: [brideMum],
+    bridedad: [brideDad],
+    brideplace: [bridePlace],
+    bridefatherjob: [brideFatherJob],
+    bridemotherjob: [brideMotherJob],
+    groomjob: [groomJob],
+    groomgrad: [groomGrad],
+    groommum: [groomMum],
+    groomdad: [groomDad],
+    groomplace: [groomPlace],
+    groommotherjob: [groomMotherJob],
+    groomfatherjob: [groomFatherJob],
+    whereplace: [wherePlace],
+    wherelocation: [whereLocation],
+    whereplace2: [wherePlace2],
+    wherelocation2: [whereLocation2],
+    origin: [
+      `
 <div id=\"results\">
 <h2>#bride#, #groom#</h2>
 
@@ -185,26 +194,25 @@ let notes = {
 <p>The couple first met #whereplace# in #wherelocation# but didn't hit it off. A later meeting #whereplace2# in #wherelocation2# proved more fruitful and the couple have been dating since 2017.</p>
 </div>
 `
-  ]
-};
+    ]
+  };
+
   var AnnounceGrammar = tracery.createGrammar(notes);
   var results = AnnounceGrammar.flatten("#origin#");
-  return results
-};
+  return results;
+}
 
-app.disable('etag')
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.disable("etag");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/", function(req, res) {
   var results = generate();
-  res.status(200).send(results);
-  res.end()
-  results = '';
+  res.send(results);
 });
 
 // listen for requests :)
